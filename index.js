3ea2559588a1aca664df092985bf9de1575e85a0
amirulkanak <coder.kanak@gmail.com>
2025-01-16 23:45:37 +0600

    jwt create and verify

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import morgan from 'morgan';
 import connectDB from './config/database.js';
 
 // Load environment variables
@@ -14,9 +16,35 @@ const allowedOrigins = process.env.FRONTEND_URLS.split(',');
 
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
+app.use(morgan('dev'));
 
 // Connect to database
 connectDB();
+
+// Routes
+// JWT Auth routes
+app.post('/jwt/create', async (req, res) => {
+  const { email } = req.body;
+  const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+    expiresIn: '3h',
+  });
+  res.send({ token });
+});
+
+// JWT Verify middleware
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1];
+  if (!token) {
+    return res.status(401).send('Unauthorized');
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if ((err, decoded)) {
+      return res.status(401).send('Unauthorized');
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 // Default route
 app.get('/', (req, res) => {
