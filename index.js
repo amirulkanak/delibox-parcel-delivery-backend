affe53d38b53cc44c6846dd6acc3405e88cd159f
amirulkanak <coder.kanak@gmail.com>
2025-01-16 13:41:11 +0600

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/database.js';
 
+// Load environment variables
+dotenv.config();
+
+// Create express app
+const app = express();
+
+// Middleware
+const allowedOrigins = process.env.FRONTEND_URLS.split(',');
+
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(express.json());
+
+// Connect to database
+connectDB();
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('Welcome to the API - developed by github/amirulkanak');
+});
+
+//  Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is online, address: http://localhost:${PORT}`);
+});
